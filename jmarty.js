9759fcbc7345369fa166a2a478becb0450f8a36a
iamsujun@gmail.com <iamsujun@gmail.com@b9c1dd4e-15ed-255f-180f-f336d9da85e5>
2010-09-10 17:24:47 +0000

    模板变量解析调整、优化

--- jmarty.js
+++ jmarty.js
@@ -8,32 +8,30 @@
  */
 (function( window, undefined ){
 var jmarty = {
-    // Engine name
-    name: 'Jmarty',
+	// Engine name
+	name: 'Jmarty',
 
-    // Version
-    version: '0.0.1',
+	// Version
+	version: '0.0.1',
 
-    // Template
+	// Template
 	Template: [],
-    current_tpl: '',
+	current_tpl: '',
 
-    // Var value list
-    valueList: [],
+	// Var value list
+	valueList: [],
 
-    // Engine set
-    ini: function( selector ){
-        alert( $.selector );
-    },
+	// Engine set
+	ini: function( selector ){
+		alert( $.selector );
+	},
 
-    // Template set
-    setTpl: function( fileName ){
-			var regExp_1 = /\{\s*(\S*?)\s*}/g;
+	// Template set
+	setTpl: function( fileName ){
 			var regExp_2 = /\{\s*include\s*file=\s*(['"])(.*?)\1\s*}/gim;//模板嵌套
 			$.current_tpl = $.getTpl( fileName )
-				.replace( regExp_1, '{$1}')
 				.replace( regExp_2, function(a,b,c){ return $.getTpl( c );} );//模板嵌套
-    },
+	},
 
 	//get Template
 	getTpl: function( fileName )
@@ -45,68 +43,52 @@ var jmarty = {
 		return $.Template[ fileName ];
 	},
 
-    // Get var RegExp
-    getRegExp: function( key ){
-        var str = '{\\$' + key + '}';
-        var reg = new RegExp( str, "gim" );
-        return reg;
-    },
+	// Get var RegExp
+	getRegExp: function( key ){
+		var str = '{\\$' + key + '}';
+		var reg = new RegExp( str, "gim" );
+		return reg;
+	},
 
-    // Assign value
-    assign: function( key, value ){
-        $.valueList[ key ] = value;
-    },
+	// Assign value
+	assign: function( key, value ){
+		$.valueList[ key ] = value;
+	},
 
-    // Set var
-    setVar: function(){
-        for ( key in $.valueList )
-        {
-            var value = $.valueList[ key ];
-            var regExp = $.getRegExp( key );
-            $.current_tpl = $.current_tpl.replace( regExp, value );
-        }
-    },
+	// Set var
+	setVar: function(){
+		reg = /{\s*\$([\w\d_]+)\s*}/gim
+		$.current_tpl = $.current_tpl.replace( reg, function( a, b ){ return $.valueList[ b ] } );
+	},
 
-    // Set foreach
-    setForeach: function(){
-        var regExp = /\{\s*foreach\s*from=\s*\$(.*?)\s*item=\s*(.*?)\s*}(.*?)\{\s*\/foreach\s*}/gim;
-        //var regExp = new RegExp( '{\s*foreach\s*from=\s*\\$(.*?)\s*item=\s*(.*?)\s*}(.*){\s*\/foreach\s*}', 'gim');
-        //RegExp.multiline = true;
-        $.current_tpl = $.current_tpl.replace(/[\r\t\n]/g, ' ').replace( regExp, function( a, b, c, d ){
-            var string='', data, value, item, reg;
-            data = $.valueList[ b ];
-            for( key in data )
-            {
-                value = data[ key ];
-                item = d;
-                for( o in value )
-                {
-                    reg = $.getRegExp(c+'.'+o);
-                    item = item.replace( reg, value[ o ] );
-                }
-                string +=item;
-            }
-            return string;
-        });
-    },
+	// Set foreach
+	setForeach: function(){
+		var regExp = /\{\s*foreach\s*from=\s*\$(.*?)\s*item=\s*(.*?)\s*}(.*?)\{\s*\/foreach\s*}/gim;
+		$.current_tpl = $.current_tpl.replace(/[\r\t\n]/g, ' ')
+			.replace( regExp, function( a, b, c, d ){
+				var string='', data, reg = /{\s*\$([\w\d_]+)\.([\w\d_]+)\s*}/gim;
+				for( key in $.valueList[ b ] )
+				{
+					data = $.valueList[ b ][key];
+					string += d.replace( reg, function( l, k, j ){ return eval( 'data.'+j); } );
+				}
+				return string;
+			});
+	},
 
-    // Display template
-    display: function(){
-        $.setVar();
-        $.setForeach();
-        document.write( $.current_tpl );
-    },
+	// Display template
+	display: function(){
+		$.setForeach();
+		$.setVar();
+		document.write( $.current_tpl );
+	},
 
-    // Debug
-    debug: function(){
-        $.setTpl( 'Jmarty author is {     $name }- {$name}, { $name }.{ $name}' );
-        $.assign( 'name', 'victor' );
-        alert( $.current_tpl );
-    },
-    assign_1: function( key, value ){
-        var reg = new RegExp( key, 'g' );
-        $.current_tpl = $.current_tpl.replace( reg, value );
-    },
+	// Debug
+	debug: function(){
+		$.setTpl( 'Jmarty author is {     $name }- {$name}, { $name }.{ $name}' );
+		$.assign( 'name', 'victor' );
+		alert( $.current_tpl );
+	},
 
 	/**
 	 * 简便Ajax请求
